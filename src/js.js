67610d318acaf79ed4e0420eece84193a35da695
Sári Cintia <sari.cintia1996@gmail.com>
2023-01-06 14:01:22 +0100

    eventHandler függvény

--- src/js.js
+++ src/js.js
@@ -1,6 +1,17 @@
 const form = document.querySelector("form");
-const location = document.querySelector("[name=location]");
+const locations = document.querySelector("[name=location]");
 const temp = document.querySelector(".temperature");
 
 form.addEventListener("submit", eventHandler);
+function getApiUrl(city){
+    return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=XHKZYQGTJ2NY7H6SNU8ED55WB&contentType=json`
+}
+function eventHandler(eventObject){
+    eventObject.preventDefault();
+    let city = locations.value.trim();
+    locations.value ='';
 
+    fetch(getApiUrl(city))
+    .then(data => data.json())
+    .then(renderResponse)
+};
