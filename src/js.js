bd40c4766d428cbe6897f0a55f8b8fe6fa9bc277
Sári Cintia <sari.cintia1996@gmail.com>
2023-01-06 14:46:37 +0100

    render celsius

--- src/js.js
+++ src/js.js
@@ -1,15 +1,34 @@
 const form = document.querySelector("form");
 const locations = document.querySelector("[name=location]");
-const temp = document.querySelector(".temperature");
+const weatherBox = document.querySelector(".js-weather-info")
+
 
 form.addEventListener("submit", eventHandler);
 function getApiUrl(city){
     return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=XHKZYQGTJ2NY7H6SNU8ED55WB&contentType=json`;
 };
 
+function renderCelsius(weather){
+    let celsius = weather.days[0].temp;
+    console.log(celsius);
+    let html = `
+    <div class="temperature">${celsius}°C</div>
+    `;
+return html;
+}
+
 function renderResponse(weather){
 console.log(weather.address)
+    let html = "";
+    //if(Array.isArray(weather) && weather.length > 0){
+        html = renderCelsius(weather)
+    //} else { // Error részt megcsinálni
+    //    html=`<p class='error'>Nem találtunk ilyen nevü várost!</p>`
+    //}
+    weatherBox.innerHTML = html;
 };
+
+
 function eventHandler(eventObject){
     eventObject.preventDefault();
     let city = locations.value.trim();
